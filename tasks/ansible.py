a560fc1dfd82133101548c7ddfa5330c5a2f2866
4235 <423545+fishg@users.noreply.github.com>
2022-01-29 12:54:26 +0800

    task 优先级调整

--- tasks/ansible.py
+++ tasks/ansible.py
@@ -3,7 +3,7 @@ from app.db.session import db_session
 from app.db.models.server import Server
 
 
-@huey.task(priority=10)
+@huey.task(priority=4)
 def ansible_hosts_runner():
     with db_session() as db:
         servers = db.query(Server).filter(Server.is_active==True).all()
